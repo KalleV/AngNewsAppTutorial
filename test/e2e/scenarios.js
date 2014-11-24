0d507e6360d4bd93b8691c384f5990f7bb6dd15e
Kalle V <bluesharkies@gmail.com>
2014-11-23 23:03:32 -0700

    Added browser.sleep() to the login error message e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -19,7 +19,6 @@
       browser.get('/#/login');
     });
 
-    // TODO: add delay to wait for the controller response before checking for the error message
     it('shows an error if the email doesn\'t exist', function() {
       var errorMessage = element(by.binding('authCtrl.error'));
       expect(errorMessage.getText()).toEqual('');
@@ -27,15 +26,8 @@
       element(by.model('authCtrl.user.email')).sendKeys('');
       element(by.model('authCtrl.user.password')).sendKeys('');
       submit.click();
-      var error = element(by.className('error'));
-      expect(error.getAttribute('class')).not.toMatch('ng-hide');
-//      expect(element(by.tagName('.p')).isDisplayed()).toBeTruthy();
-      //browser.findElement(by.binding('authCtrl.error')).then(function(elt) {
-      //browser.findElement(by.css('.p')).then(function(elt) {
-      //  expect(elt.evaluate('authCtrl.error'))
-      //    .toEqual('Error: The specified email address is incorrect.');
-      //});
-      //expect(element(by.binding('authCtrl.error')).getText()).not.toEqual('');
+      browser.sleep(2000);  // wait for $firebaseAuth to respond
+      expect(element(by.binding('authCtrl.error')).getText()).not.toEqual('');
     });
   })
 
